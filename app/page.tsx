1229094be91ad30fad4f354f11da66a8dd702222
Favour <jammoben@gmail.com>
2025-01-15 15:27:02 +0000

    Fixed an issue with the background loading

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,23 @@ import WindowsRightClick from "@/components/WindowsRightClick";
 import { useDataStore } from "@/store/data";
 import { useEffect } from "react";
 import { StoreDataType, AppType } from "..";
-
 export default function Home() {
   return (
-    <div className="relative h-screen bg-[url('../public/images/wallpapers/wallpaper-purple.jpg')] bg-rose-500 bg-cover overflow-hidden">
+    <div
+      style={{
+        backgroundImage: "url('/images/wallpapers/wallpaper-purple.jpg')",
+      }}
+      className="relative h-screen bg-rose-500 bg-cover overflow-hidden"
+    >
       <main className="w-full h-full relative">
         <FileExplorer />
         <WindowsRightClick />
-        <div className="h-full w-fit min-w-[10%] px-8 pb-10 pt-5 flex flex-wrap gap-y-5 gap-x-5 justify-start flex-col items-start">
+        <div className="h-full w-fit min-w-[10%] px-8 pb-10 pt-5 flex flex-wrap gap-y-4 gap-x-5 justify-start flex-col items-start">
           {new Array(10).fill(0).map((value: number, idx: number) => {
             return (
-              <button key={idx}>
+              <div key={idx}>
                 <Folder />
-              </button>
+              </div>
             );
           })}
         </div>
@@ -56,7 +60,7 @@ function BottomTaskBar() {
 
 function Folder() {
   return (
-    <>
+    <button className="hover:bg-[rgb(255,255,255,0.1)] p-2 rounded-md transition-all duration-100">
       <Image
         alt="folder icon 2"
         src={"/icons/folder-and-files/empty-folder.png"}
@@ -64,6 +68,6 @@ function Folder() {
         height={48}
       ></Image>
       <p className="font-openSans text-sm font-semibold">Folder</p>
-    </>
+    </button>
   );
 }
